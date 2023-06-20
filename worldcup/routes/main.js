f0be81ea1077dfaf0886e9dc5fd136319811f2b7
leegyurye <127065157+leegyurye@users.noreply.github.com>
2023-06-20 18:42:29 +0900

    upload

--- worldcup/routes/main.js
+++ worldcup/routes/main.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(morgan('dev'));
 
 var db;
-var databaseUrl = 'mongodb://13.124.53.50:27017';
+var databaseUrl = 'mongodb://13.124.4.168:27017';
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + './index.html');
@@ -17,28 +17,37 @@ app.get('/', (req, res) => {
 
 app.get('/worldcup', (req, res) => {
 	console.log(mongoClient)
-	mongoClient.connect(databaseUrl, function(err, database)) {
-	if (err != null) 
+	mongoClient.connect(databaseUrl, function(err, database) {
+	if (err != null) {
 		res.json({'count':0});
 		console.log(err);
-	} else {
-		db.database.db('test')
-		db.collection('worldcup').find({}, {_id:0, no:1, nation:1, date:1}).toArray(function(err, result))
-	}
-		if(err) throw err;
-		console.log('result: ');
-		console.log(result);
-		res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
-		var template = `
-			<>
-			`
-
-
-
-
-
-
-		
-})
+	} else {	
+		db = database.db('test');
+		db.collection('worldcup').find({}, {_id:0, no:1, nation:1, date:1}).toArray(function(err, result) {
+				if (err) throw err;
+				console.log('result: ');
+				console.log(result);
+				res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+				var template = `
+					<table border="1" margin:auto; text-align:center;>
+					<tr>
+						<th> No </th>
+						<th> Nation </th>
+						<th> Date </th>
+					</tr> `;
+					result.forEach((item) => {
+						template += `
+						<tr>
+							<th> ${item.no} </th>
+							<th> ${item.nation} </th>
+							<th> ${item.date} </th>
+						</tr> `;
+					});
+					template += `</table>`;
+				res.end(template);
+			});
+		}
+	});
+});
 
 module.exports = app;
